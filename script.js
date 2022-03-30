b27ec29940e3aca61e47b8a635758b9585ca1e2b
João Alfredo Alves <68473607+JoaoAlfredoAlves@users.noreply.github.com>
2022-03-30 15:13:25 -0300

    Construindo a logica do script js

---
+++ script.js
@@ -0,0 +1,88 @@
+const texto = document.querySelector("input");
+const btnInsert = document.querySelector(".divInsert button");
+const btnDeleteAll = document.querySelector(".header button");
+const ul = document.querySelector("ul");
+
+let itensDB = [];
+
+btnDeleteAll.onclick = () => {
+	itensDB = [];
+	updateDB();
+};
+
+texto.addEventListener("keypress", (e) => {
+	if (e.key === "Enter" && text.value !== "") {
+		setItemDB();
+	}
+});
+
+btnInsert.onclick = () => {
+	if (texto.value !== "") {
+		setItemDB();
+	}
+};
+
+function setItemDB() {
+	if (itensDB.length >= 20) {
+		alert("Limite de itens atingido!");
+		return;
+	}
+
+	itensDB.push({ item: texto.value, status: "" });
+	updateDB();
+}
+
+function updateDB() {
+	localStorage.setItem("toDoList", JSON.stringify(itensDB));
+	loadItens();
+}
+
+function loadItens() {
+	ul.innerHTML = "";
+	itensDB = JSON.parse(localStorage.getItem("toDoList")) ?? [];
+	itensDB.forEach((item, i) => {
+		insertItemTela(item.item, item.status, i);
+	});
+}
+
+function insertItemTela(text, status, i) {
+	const li = document.createElement("li");
+
+	li.innerHTML = `
+    <div class="divLi">
+      <input type="checkbox" ${status} data-i=${i} onchange="done(this, ${i});" />
+      <span data-si=${i}>${text}</span>
+      <button onclick="removeItem(${i})" data-i=${i}><i class='bx bx-trash'></i></button>
+    </div>
+    `;
+	ul.appendChild(li);
+
+	if (status) {
+		document
+			.querySelector(`[data-si="${i}"]`)
+			.classList.add("line-through");
+	} else {
+		document
+			.querySelector(`[data-si="${i}"]`)
+			.classList.remove("line-through");
+	}
+
+	texto.value = "";
+}
+
+function done(chk, i) {
+	if (chk.checked) {
+		itensDB[i].status = "checked";
+	} else {
+		itensDB[i].status = "";
+	}
+
+	updateDB();
+}
+
+function removeItem(i) {
+	itensDB.splice(i, 1);
+	updateDB();
+}
+
+loadItens();
